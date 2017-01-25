9a8c6cdf357bfd1a00490931d3680ce050fd7c21
M4rtin <martin.laadoga@itcollege.ee>
2017-01-25 14:12:45 +0200

    Create eksam9-12.java

---
+++ src/eksam9-12.java
@@ -0,0 +1,52 @@
+Isendimuutujate väärtused määravad:
+
+
+Values of instance variables determine:
+Vali üks:
+a. objekti oleku; object state
+b. objekti klassi; object class
+c. objekti käitumise; object behaviour
+d. objekti identiteedi; object identity
+Küsimus 10
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Mitmene pärimine tähendab, et:
+
+
+Multiple inheritance means that
+Vali üks:
+a. klass võib omadusi pärida mitmelt ülemklassilt; a class can inherit properties from several superclasses
+b. iga klass saab olla ülimalt ühe klassi alamklass; each class can be a subclass of at most one class
+c. teate poolt aktiviseeritav meetod valitakse programmi lahendamise ajal; the method activated by a message is chosen during runtime
+d. teate poolt aktiviseeritav meetod valitakse programmi kompileerimise ajal; the method activated by a message is chosen during compile time
+Küsimus 11
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Java koodistiil nõuab klassinimede kirjutamist:
+
+
+Class names in Java are written:
+Vali üks:
+a. ainult väikeste tähtedega; with all lowercase letters
+b. suure algustähega; starting with capital letter
+c. ainult suurtähtedega; with all capital letters
+d. väikese algustähega; starting with lowercase letter
+Küsimus 12
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Javadoc @since 
+
+abil dokumenteeritakse:
+
+is used to document:
+Vali üks:
+a. varaseimat Java kompilaatori versiooni, millega klass kompileerub; earliest Java compiler version to compile the class
+b. Java versiooni, millest alates seda klassi ei toetata; Java version starting form which this class is depricated
+c. hetkel kasutatavat Java kompilaatori versiooni; current Java compiler version
+d. klassi versiooniinfot; version information of the class
