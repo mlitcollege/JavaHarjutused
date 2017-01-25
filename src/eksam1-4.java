1bdaf461311c5de90b47f25f149c513c5366c28e
M4rtin <martin.laadoga@itcollege.ee>
2017-01-25 14:10:48 +0200

    Create eksam1-4.java

---
+++ src/eksam1-4.java
@@ -0,0 +1,53 @@
+Kõrgtaseme keeles kirjutatud programmi teisendab masinkoodiks... 
+Computer program written in high level language is converted to machine code by...
+Vali üks:
+a. kompilaator; compiler
+b. API
+c. assembler
+d. interpretaator; interpreter
+Küsimus 2
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Mida tähistab keeles Java järgmine kirjutis? 
+
+What does the following mean in Java?
+
+ -0xaL 
+Vali üks:
+a. 32-bitine täisarv -10 ; 32-bit integer -10
+b. Ei ole Javas lubatud; is not allowed in Java
+c. 64-bitine täisarv -10 ; 64-bit integer -10
+d. 16-bitine täisarv -10 ; 16-bit integer -10
+Küsimus 3
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Milline järgnevatest keele Java tüüpidest on algtüüp (primitive type)?
+
+Which of the following is a primitive type in Java?
+Vali üks:
+a. String
+b. char
+c. Integer
+d. Boolean
+Küsimus 4
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Millise tulemuse annab järgmine programmilõik? What is the result of the following program?
+ 
+
+int a = 4;
+
+int b = a++ / 5;
+
+ 
+Vali üks:
+a. a==5 && b==1
+b. a==5 && b==0.8
+c. a==5 && b==0
+d. a==4 && b==0
