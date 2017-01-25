63041e444ffdab55346fbc324e820250b027b5ac
M4rtin <martin.laadoga@itcollege.ee>
2017-01-25 14:13:39 +0200

    Create eksam13-16.java

---
+++ src/eksam13-16.java
@@ -0,0 +1,50 @@
+Liides Comparable keeles Java võimaldab:
+
+Comparable interface in Java is used to
+Vali üks:
+a. küsida andmekogumi järgmist elementi; ask the next element of a collection
+b. lisada hulka korduvaid elemente; add multiple elements to the set
+c. otsustada, kumb kahest elemendist on suurem; decide which of the two elements is greater
+d. salvestada paare "võti-väärtus"; store the "key - value" pairs
+Küsimus 14
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Kogumi elementide süstemaatiliseks läbikäimiseks sobib Java APIs liides:
+
+
+Which of the following Java interfaces is suitable for systematic walk-through of collection elements
+Vali üks:
+a. Set
+b. Map
+c. Iterator
+d. Comparable
+Küsimus 15
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Eriolukorda, mille töötlemine programmeerija poolt on võimalik, aga mitte kohustuslik, kirjeldab Java klass:
+
+
+Handling the instances of ... is not compulsory (but still possible) in Java.
+Vali üks:
+a. RuntimeException
+b. IOException
+c. Error
+d. Exception
+Küsimus 16
+Vastus on salvestatud
+Võimalik punktisumma 1,00'st
+ Mitte märgistatudMärgista küsimus
+Küsimuse tekst
+Tekstivoo lugemisel meetodiga readLine() annab voo lõppemisest märku:
+
+
+Text stream is read using the readLine() method. End of stream is reported as
+Vali üks:
+a. (int)0
+b. (String)null
+c. (int)-1
+d. "\n"
