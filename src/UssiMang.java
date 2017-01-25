fa64d71f7fc1d368c31f621415426770630986b5
M4rtin <martin.laadoga@itcollege.ee>
2017-01-25 08:30:10 +0200

    Update UssiMang.java

--- src/UssiMang.java
+++ src/UssiMang.java
@@ -1,12 +1,8 @@
 // Kokkupõrge - sein
 if (ussiPea.getTranslateX() < 0 || ussiPea.getTranslateX() >= aknaLaius) { 
-	restartGame();
+	manguRestart();
 }
 // Kokkupõrge - lagi/põrand
 if (ussiPea.getTranslateY() < 0 || ussiPea.getTranslateY() >= aknaKorgus) { 
-	restartGame();
+	manguRestart();
 }
-50 Node
-// (if) Kui seal on rohkem kui kaks blokki {siis eemaldame "ussilt" (saba) viimase bloki}.
-// (else) ja kui mitte, siis saame (get) 0 elemendi "ussilt", mis on siis ussipea. 
-// "Node" on listi elemendi tüüp (ese)
